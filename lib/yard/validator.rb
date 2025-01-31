d63efea3056b7c7bc288697c5461497f8bcbd081
Herwin Weststrate <herwin@quarantainenet.nl>
2025-01-31 13:42:15 +0100

    Add barebones Gemfile

---
+++ lib/yard/validator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "validator/version"
+
+module Yard
+  module Validator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
